692f274297d852fdb549659be82f0d4184b9974e
Nate Tryon <natetryon@gmail.com>
2021-04-21 13:53:50 -0400

    All JS functions in, need to rewrite player solve

--- wwwroot/js/maze.js
+++ wwwroot/js/maze.js
@@ -4,7 +4,14 @@ const SVG_HEIGHT = 900;
 const CH_WALL = 'X';
 const CH_SPACE = "-";
 
-let MazeGrid = [];
+let raceTimer = 0;
+let solution = null;
+let sol = null;
+let sol2 = null;
+let solutionVisible = false;
+let player = null;
+let raceInProgress = false;
+let raceTimeoutID = null;
 
 var Maze = {
 
@@ -29,7 +36,7 @@ var Maze = {
 
 };
 
-function ActivateButtons() {
+function activateButtons() {
 
     document.getElementById("button_solve").removeAttribute("disabled");
     document.getElementById("button_solution").removeAttribute("disabled");
@@ -37,7 +44,49 @@ function ActivateButtons() {
 
 }
 
-function DrawHorizontal(x, y, length, thickness, color)
+function beginRace() {
+
+    if (!raceInProgress)
+    {
+        document.getElementById("button_solve").setAttribute("disabled", true);
+        if (sol != null ) sol.hide();
+        if (sol2 != null) sol2.hide();
+        let roll = getRandom(3);
+        sol = new Solver(Maze, (roll + 1) % 3, "solverOne");
+        sol2 = new Solver(Maze, (roll + 2) % 3, "solverTwo");
+
+        race();
+    }
+
+    else
+    {
+        console.log("Race currently in progress");
+        setText("Race currently in progress");
+    }
+}
+
+function computerSolve() {
+
+    if (!solutionVisible)
+    {
+        solution = new Solver(Maze, 1, "compSol");
+        solution.solveMaze();
+        solution.draw();
+
+        solutionVisible = true;
+        document.getElementById("button_solution").innerHTML = "Hide Solution"; 
+    }
+
+    else
+    {
+        solution.hide();
+        solutionVisible = false;
+        document.getElementById("button_solution").innerHTML = "Show Solution"; 
+    }
+
+}
+
+function drawHorizontal(x, y, length, thickness, color)
 {
     let el = document.createElementNS(SVG_NAMESPACE, "rect");
     let svg = document.getElementById("mazeSVG");
@@ -53,7 +102,61 @@ function DrawHorizontal(x, y, length, thickness, color)
 
 }
 
-function DrawVertical(x, y, length, thickness, color)
+function drawPlayer() {
+
+    let el = document.createElementNS(SVG_NAMESPACE, "polyline");
+    let el_tip1 = document.createElementNS(SVG_NAMESPACE, "circle");
+    let el_tip2 = document.createElementNS(SVG_NAMESPACE, "circle");
+    let svg = document.getElementById("mazeSVG");
+    
+    let elPrevious = document.getElementById("playerPL");
+    let elPrevious_t1 = document.getElementById("player_t1");
+    let elPrevious_t2 = document.getElementById("player_t2");
+
+    if (elPrevious != null) svg.removeChild(elPrevious);
+    if (elPrevious_t1 != null) svg.removeChild(elPrevious_t1);
+    if (elPrevious_t2 != null) svg.removeChild(elPrevious_t2);
+
+    if (player == null) return;
+
+
+    let pointsStr = "";
+
+    for (let pt of player.points)
+    {
+        let px = Maze.columnPixels * (pt.x + 1 / 2);
+        let py = Maze.rowPixels * (pt.y + 1 / 2);
+        pointsStr += (px + "," + py + " ");
+    }
+
+    el.setAttribute("points", pointsStr);
+    el.setAttribute("fill", "none");
+    el.setAttribute("stroke", playerColor.getCode());
+    el.setAttribute("stroke-width", Maze.columnPixels);
+    el.setAttribute("id", "playerPL");
+    svg.appendChild(el);
+
+    let t1_x = Maze.columnPixels * (player.points[0].x + 1 / 2);
+    let t1_y = Maze.rowPixels * (player.points[0].y + 1 / 2);
+    el_tip1.setAttribute("cx", t1_x);
+    el_tip1.setAttribute("cy", t1_y);
+    el_tip1.setAttribute("r", Maze.columnPixels / 2);
+    el_tip1.setAttribute("fill", playerColor.getCode());
+    el_tip1.setAttribute("id", "player_t1");
+    svg.appendChild(el_tip1);
+
+    let t2_x = Maze.columnPixels * (player.points[player.points.length - 1].x + 1 / 2);
+    let t2_y = Maze.rowPixels * (player.points[player.points.length - 1].y + 1 / 2);
+    el_tip2.setAttribute("cx", t2_x);
+    el_tip2.setAttribute("cy", t2_y);
+    el_tip2.setAttribute("r", Maze.columnPixels / 2);
+    el_tip2.setAttribute("fill", playerTipColor.getCode());
+    el_tip2.setAttribute("id", "player_t2");
+    svg.appendChild(el_tip2);
+
+}
+
+function drawVertical(x, y, length, thickness, color)
 {
     let el = document.createElementNS(SVG_NAMESPACE, "rect");
     let svg = document.getElementById("mazeSVG");
@@ -69,7 +172,128 @@ function DrawVertical(x, y, length, thickness, color)
 
 }
 
-function PaintBackground(color) {
+function getRandom(n) {
+    return Math.floor(Math.random() * n);
+}
+
+function keyDown(event) {
+
+    event.preventDefault();
+
+    player.direction = 0;
+    let checkX = player.points[player.points.length - 1].x;
+    let checkY = player.points[player.points.length - 1].y;
+
+    if (checkX == Maze.exitX && checkY == Maze.exitY)
+    {
+        console.log("Maze solved!");
+        setText("Great job, you solved the maze!");
+        window.removeEventListener("keydown", keyDown, true);
+        return;
+    }
+
+    switch (event.keyCode)
+    {
+        case 37:
+        {
+            player.direction = 3;
+            --checkX;
+            // console.log("Movement left!");
+        } break;
+
+        case 38:
+        {
+            player.direction = 1;
+            --checkY;
+            // console.log("Movement up!");
+        } break;
+
+        case 39:
+        {
+            player.direction = 4;
+            ++checkX;
+            // console.log("Movement right!");
+        } break;
+
+        case 40:
+        {
+            player.direction = 2;
+            ++checkY;
+            // console.log("Movement down!");
+        } break;
+
+        default: {} break;
+    }
+
+    if (checkX < 0 || checkY < 0) return;
+
+    if (player.points.length > 2 &&
+        (checkX == player.points[player.points.length - 2].x &&
+        checkY == player.points[player.points.length - 2].y) )
+    {
+        player.points.pop();
+    }
+
+    else if (player.direction > 0 && Maze.grid[checkY][checkX] !== CH_WALL)
+        player.grow();
+
+    drawPlayer();
+
+}
+
+function keyUp(event) {
+    player.direction = 0;
+}
+
+function mouse(event) {
+
+    player.direction = 0;
+    let checkX = player.points[player.points.length - 1].x;
+    let checkY = player.points[player.points.length - 1].y;
+
+    if (event.movementX > 1)
+    {
+        player.direction = 4;
+        ++checkX;
+        console.log("Movement right!");
+    }
+
+    if (event.movementX < -1)
+    {
+        player.direction = 3;
+        --checkX;
+        console.log("Movement left!");
+    }
+
+    if (event.movementY > 1)
+    {
+        player.direction = 2;
+        ++checkY;
+        console.log("Movement down!");
+    }
+
+    if (event.movementY < -1)
+    {
+        player.direction = 1;
+        --checkY;
+        console.log("Movement up!");
+    }
+
+    if (player.points.length > 2 &&
+        checkX == player.points[player.points.length - 2].x &&
+        checkY == player.points[player.points.length - 2].y)
+    {
+        player.points.pop();
+    }
+
+    else if (player.direction > 0 && Maze.grid[checkY][checkX] !== CH_WALL)
+        player.grow();
+
+    drawPlayer();
+
+}
+
+function paintBackground(color) {
 
     let el = document.createElementNS(SVG_NAMESPACE, "rect");
     let svg = document.getElementById("mazeSVG");
@@ -89,11 +313,11 @@ function PaintBackground(color) {
 
 }
 
-function PaintMaze(spaceColorString, wallColorString) {
+function paintMaze(spaceColorString, wallColorString) {
 
     let svg = document.getElementById("mazeSVG");
     svg.innerHTML = "";
-    PaintBackground(spaceColorString);
+    paintBackground(spaceColorString);
 
     // Horizontal wall sections
     for (let i = 0; i < Maze.rows; ++i)
@@ -116,7 +340,7 @@ function PaintMaze(spaceColorString, wallColorString) {
             {
                 if (sectionLength > 1)
                 {
-                    DrawHorizontal(
+                    drawHorizontal(
                         sectionX,
                         sectionY,
                         sectionLength * Maze.columnPixels,
@@ -150,7 +374,7 @@ function PaintMaze(spaceColorString, wallColorString) {
             {
                 if (sectionLength > 1)
                 {
-                    DrawVertical(
+                    drawVertical(
                         sectionX,
                         sectionY,
                         sectionLength * Maze.rowPixels,
@@ -164,7 +388,53 @@ function PaintMaze(spaceColorString, wallColorString) {
     }
 }
 
-function SetMaze(gridString, complexity) {
+function playerSolve() {
+
+    setText("Use arrow keys to solve the maze.");
+    player = new WallPath(Maze.startX, Maze.startY);
+    playerColor = new ColorRGB(255, 20, 147);
+    playerTipColor = new ColorRGB(0, 255, 255);
+    player.grow();
+
+    // window.addEventListener("mousemove", mouse, true);
+    window.addEventListener("keydown", keyDown, true);
+    document.getElementById("mazeSVG").addEventListener("touchmove", touch, false);
+
+    drawPlayer();
+
+}
+
+function pointEquals(p1, p2)
+{
+    return (p1.x == p2.x && p1.y == p2.y);
+}
+
+function race() {
+
+    sol.draw();
+    sol.iterate();
+
+    sol2.draw();
+    sol2.iterate();
+
+    if (!sol.isSolved() && !sol2.isSolved())
+    {
+        raceInProgress = true;
+        raceTimeoutID = window.setTimeout(race, 5);
+    }
+
+    else
+    {
+        if (sol.isSolved()) sol.draw();
+        if (sol2.isSolved()) sol2.draw();
+
+        raceInProgress = false;
+        document.getElementById("button_solve").removeAttribute("disabled");
+    }
+
+}
+
+function setMaze(gridString, complexity) {
 
     Maze.grid = [];
 
@@ -210,4 +480,12 @@ function SetMaze(gridString, complexity) {
         }
     }
 
+}
+
+function setText(str) {
+    document.getElementById("maze-text").innerText = str;
+}
+
+function touch(event) {
+    event.preventDefault();
 }
