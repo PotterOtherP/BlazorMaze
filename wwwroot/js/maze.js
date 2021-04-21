ac3c5f079c23da462dfee3019ee83cb8d0e07fbc
Nate Tryon <natetryon@gmail.com>
2021-04-21 03:57:16 -0400

    Maze JS object created from string

--- wwwroot/js/maze.js
+++ wwwroot/js/maze.js
@@ -4,6 +4,28 @@ const SVG_HEIGHT = 900;
 const CH_WALL = 'X';
 const CH_SPACE = "-";
 
+let MazeGrid = [];
+
+var Maze = {
+
+    grid: [],
+    startX: 0,
+    startY: 0,
+    exitX: 0,
+    exitY: 0,
+
+    complexity: 0,
+
+    rows: 0,
+    columns: 0,
+
+    wall_left: 0,
+    wall_top: 0,
+    wall_right: 0,
+    wall_bottom: 0
+
+};
+
 function DrawHorizontal(x, y, length, thickness, color)
 {
     let el = document.createElementNS(SVG_NAMESPACE, "rect");
@@ -56,54 +78,27 @@ function PaintBackground(color) {
 
 }
 
-function GetMazeGridFromString(gridString) {
+function PaintMaze(spaceColorString, wallColorString) {
 
-    let grid = [];
-
-    var j = 0;
-    grid.push([]);
-
-    for (var i = 0; i < gridString.length - 1; ++i)
-    {
-        if (gridString[i] == '|')
-        {
-            grid.push([]);
-            ++j;
-        }
-
-        else if (gridString[i] == CH_SPACE || gridString[i] == CH_WALL)
-        {
-            grid[j].push(gridString[i]);
-        }
-    }
-
-    return grid;
-
-}
-
-function PaintMaze(gridString, spaceColorString, wallColorString) {
-
-    let grid = GetMazeGridFromString(gridString);
-
-    let rows = grid.length;
-    let columns = grid[0].length;
-    let rowPixels = SVG_HEIGHT / rows;
-    let columnPixels = SVG_WIDTH / columns;
+    let rows = Maze.grid.length;
+    let columns = Maze.grid[0].length;
+    let rowPixels = SVG_HEIGHT / Maze.rows;
+    let columnPixels = SVG_WIDTH / Maze.columns;
     
     let svg = document.getElementById("mazeSVG");
     svg.innerHTML = "";
     PaintBackground(spaceColorString);
 
     // Horizontal wall sections
-    for (let i = 0; i < rows; ++i)
+    for (let i = 0; i < Maze.rows; ++i)
     {
         let sectionLength = 0;
         let sectionX = 0;
         let sectionY = i * rowPixels;
 
-        for (let j = 0; j < columns; ++j)
+        for (let j = 0; j < Maze.columns; ++j)
         {
-            if (grid[i][j] == CH_WALL)
+            if (Maze.grid[i][j] == CH_WALL)
             {
                 if (sectionLength == 0)
                     sectionX = j * columnPixels;
@@ -111,7 +106,7 @@ function PaintMaze(gridString, spaceColorString, wallColorString) {
                 ++sectionLength;
             }
 
-            if (grid[i][j] != CH_WALL || j == columns - 1)
+            if (Maze.grid[i][j] != CH_WALL || j == Maze.columns - 1)
             {
                 if (sectionLength > 1)
                 {
@@ -124,15 +119,15 @@ function PaintMaze(gridString, spaceColorString, wallColorString) {
     }
 
     // Vertical wall sections
-    for (let i = 0; i < columns; ++i)
+    for (let i = 0; i < Maze.columns; ++i)
     {
         let sectionLength = 0;
         let sectionX = i * columnPixels;
         let sectionY = 0;
 
-        for (let j = 0; j < rows; ++j)
+        for (let j = 0; j < Maze.rows; ++j)
         {
-            if (grid[j][i] == CH_WALL)
+            if (Maze.grid[j][i] == CH_WALL)
             {
                 if (sectionLength == 0)
                     sectionY = j * rowPixels;
@@ -140,7 +135,7 @@ function PaintMaze(gridString, spaceColorString, wallColorString) {
                 ++sectionLength;
             }
 
-            if (grid[j][i] != CH_WALL || j == rows - 1)
+            if (Maze.grid[j][i] != CH_WALL || j == Maze.rows - 1)
             {
                 if (sectionLength > 1)
                 {
@@ -151,4 +146,49 @@ function PaintMaze(gridString, spaceColorString, wallColorString) {
             }
         }
     }
+}
+
+function SetMaze(gridString, complexity) {
+
+    Maze.grid = [];
+
+    var j = 0;
+    Maze.grid.push([]);
+
+    for (var i = 0; i < gridString.length - 1; ++i)
+    {
+        if (gridString[i] == '|')
+        {
+            Maze.grid.push([]);
+            ++j;
+        }
+
+        else if (gridString[i] == CH_SPACE || gridString[i] == CH_WALL)
+        {
+            Maze.grid[j].push(gridString[i]);
+        }
+    }
+    
+    Maze.complexity = complexity;
+    Maze.rows = complexity % 2 == 1 ? complexity * 3 : (complexity * 3) + 1;
+    Maze.columns = (complexity * 4) + 1;
+
+    Maze.wall_right = Maze.columns - 1;
+    Maze.wall_bottom = Maze.rows - 1;
+
+    for (var i = 0; i < Maze.grid[0].length; ++i)
+    {
+        if (Maze.grid[0][i] == CH_SPACE)
+        {
+            Maze.startX = i;
+            Maze.startY = 0;
+        }
+
+        if (Maze.grid[Maze.rows - 1][i] == CH_SPACE)
+        {
+            Maze.exitX = i;
+            Maze.exitY = Maze.rows - 1;
+        }
+    }
+
 }
